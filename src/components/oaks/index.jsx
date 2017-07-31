65f9c04f99a606728a422d0074e6752e480945b9
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2017-07-31 09:00:15 -0400

    get proof of concept oak dropdown working

--- src/components/oaks/index.jsx
+++ src/components/oaks/index.jsx
@@ -1,23 +1,46 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { getAllOaks } from 'coda/services/oaks';
+import Select from 'react-select';
+import autobind from 'react-autobind';
+
+import 'react-select/dist/react-select.css';
 
 export default class Oaks extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      oaks: []
+      oaks: [],
+      formattedOaks: [],
+      selected: undefined,
+      selectedOak: {}
     };
+    autobind(this);
   }
 
   componentWillMount() {
-    getAllOaks().then(oaks => this.setState({ oaks }));
+    getAllOaks().then(oaks => {
+      let formattedOaks = oaks.map(o => ({ value: o.id, label: `${o.genus} ${o.species} ${o.commonName? `(${o.commonName})` : ''}` }));
+      this.setState({ oaks, formattedOaks });
+    });
+  }
+
+  onOakSelected(option) {
+    let selectedOak = this.state.oaks.find(o => o.id === option.value);
+    this.setState({ selected: option, selectedOak });
   }
 
   render() {
+    let { formattedOaks, selectedOak, selected } = this.state;
     return (
       <div>
         <h4>This will be the oaks page</h4>
-        {this.state.oaks.length ? this.state.oaks.map(oak => <div key={oak.id}>{oak.genus} {oak.species}</div>) : null}
+        <Select
+          options={formattedOaks}
+          onChange={(option) => this.onOakSelected(option)}
+          value={selected}
+        />
+
+        <h4>You have selected {selectedOak.genus} {selectedOak.species}</h4>
       </div>
     );
   }
