bd2f936011f19fceb8e94b3944ce3e2524c49d78
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2017-08-02 11:42:01 -0400

    decode notes in arraybuffer from agent

--- src/services/agents.js
+++ src/services/agents.js
@@ -1,4 +1,4 @@
-// import { arrayBufferToString } from './utils';
+import { arrayBufferToString } from './utils';
 
 const url = 'http://localhost:3000'; // TODO per environment setup
 
@@ -12,14 +12,34 @@ export function getAllAgentSynonyms() {
         return [];
       }
     })
-    // .then(oaks => {
-    //   oaks.forEach(oak => {
-    //     oak.notes = arrayBufferToString(oak.notes.data);
-    //   });
-    //   return oaks;
-    // })
+    .then(agents => {
+      agents.forEach(agent => {
+        agent.commonName = agent.agent.commonName;
+      });
+      return agents;
+    })
     .catch(err => {
       console.warn(err);
       return [];
+    });
+}
+
+export function getAgent(id) {
+  return fetch(`${url}/agent/${id}`, { mode: 'cors' })
+    .then(res => {
+      if(res.ok) {
+        return res.json();
+      } else {
+        console.warn(res);
+        return {};
+      }
+    })
+    .then(agent => {
+      agent.notes = arrayBufferToString(agent.notes.data);
+      return agent;
+    })
+    .catch(err => {
+      console.warn(err);
+      return {};
     });
 }
