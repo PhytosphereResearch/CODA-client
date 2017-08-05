31454e376a6f1681d404a63be0d534e8727ccfb5
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2017-08-04 20:42:21 -0400

    Updated interaction rendering to display interaction numbers.

--- src/components/interactions/index.jsx
+++ src/components/interactions/index.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import autobind from 'react-autobind';
 import Select from 'react-select';
 import { getInteractions } from 'coda/services/interactions';
+import SearchResult from './SearchResult.jsx'
 const plantParts = ['acorn', 'branch', 'leaf', 'trunk', 'flower', 'root'];
 
 export default class Interactions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: {}
+      selected: {},
+      interactions: []
     };
     autobind(this);
   }
@@ -17,15 +19,20 @@ export default class Interactions extends Component {
     this.setState({ selected: { ...this.state.selected, [key]: option } });
   }
 
-  onSearchClick(){
-    let { selected } = this.state
+  onSearchClick() {
+    this.setState({ searching: true });
+    let { selected } = this.state;
     getInteractions(selected.plantPart, selected.symptom.value, selected.oak.value)
-      .then(res => console.log(res)) 
+    .then(interactions => {
+      this.setState({ searching: false });
+      this.setState({ interactions })
+      console.log(interactions);
+    });
   }
 
   render() {
     let { oaks, symptoms } = this.props
-    let { selected } = this.state
+    let { selected, searching, interactions } = this.state
     return (
       <div>
         <h2>Find a Host/Agent Interaction</h2>
@@ -60,12 +67,13 @@ export default class Interactions extends Component {
             />
             <button
               className="search-button"
-              onClick = {this.onSearchClick}>
+              onClick={this.onSearchClick}>
              Search this combination
             </button>
           </div>
           <div>
-
+            { searching ? 'searching...' : '' }
+            { interactions.map( interaction => <SearchResult interaction={interaction}/>}
           </div>
         </div>
       </div>
