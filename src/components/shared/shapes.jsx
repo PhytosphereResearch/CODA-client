485e5501f63203883ec78012d31e6f59e9e78ca0
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2017-08-05 18:40:22 -0400

    make simple svg x

---
+++ src/components/shared/shapes.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const X = () => (
+  <svg width="15" height="15">
+    <line x1="0" y1="0" x2="15" y2="15" strokeWidth="2"/>
+    <line x1="15" y1="0" x2="0" y2="15" strokeWidth="2"/>
+  </svg>
+);
