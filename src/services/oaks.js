cdd36313890a1f47ad24f188b8b3a67e21bfdf5c
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2017-08-11 19:47:49 -0400

    fix broken USDA plants range link by porting oak code

--- src/services/oaks.js
+++ src/services/oaks.js
@@ -1,33 +1,36 @@
-import { arrayBufferToString } from './utils';
+import { arrayBufferToString, checkResponse } from './utils';
 
 const url = 'http://localhost:3000'; // TODO per environment setup
 
 export const getAllOaks = () => {
   return fetch(`${url}/oaks`, { mode: 'cors' })
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        console.warn(res);
-        return [];
-      }
-    })
+    .then(checkResponse)
     .catch(err => {
       console.warn(err);
       return [];
     });
 };
 
+export const getPlantCode = (oak) => {
+  var qstring = `Genus=${oak.genus}&Species=${oak.species}&Variety=${oak.subSpecies}&limit=1`;
+  return fetch('https://plantsdb.xyz/search?' + qstring)
+    .then(checkResponse)
+    .then(res => res.data[0].Accepted_Symbol_x)
+    .then(code => {
+      oak.code = code;
+      return oak;
+    })
+    .catch(err => {
+      console.warn(err);
+      oak.code = '';
+      return oak;
+    });
+};
+
 export const getOak = (id) => {
   return fetch(`${url}/oaks/${id}`, { mode: 'cors' })
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      console.warn(res);
-      return {};
-    }
-  })
+  .then(checkResponse)
+  .then(getPlantCode)
   .then(oak => {
     oak.notes = arrayBufferToString(oak.notes.data).replace(/ -/g, '\n-');
     return oak;
