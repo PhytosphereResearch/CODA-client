79de472c365f0bfc4f4c9f8239dd2354a856ef20
Douglas Salazar <countertenor.salazar@gmail.com>
2018-02-17 18:19:21 -0500

    Add reminder to add new references before adding dependent interactions

--- src/components/edit/HiEntry.jsx
+++ src/components/edit/HiEntry.jsx
@@ -12,54 +12,56 @@ const HiEntry = ({
   onAgentSelected, onOakSelected, getHi, onInputChange, onMultiInputChange,
   onBibSelectChange, onSubsiteSelectChange, onHisymptomMultiInputChange, onHisymptomRadioChange,
   onMapChange, onSymptomChange, onSymptomRemove, onHiSubmit, addHiSymptom, newHi,
-}) => (
-  <div>
-    <h3>Host-Agent Interactions</h3>
-    <h4>Find an agent</h4>
-    <Select
-      options={agents}
-      onChange={onAgentSelected}
-      value={selectedAgent}
-      placeholder="Type to search by species or common name"
-      style={{ marginBottom: '15px' }}
-    />
-    <h4>Find an oak</h4>
-    <Select
-      options={oaks}
-      onChange={onOakSelected}
-      value={selectedOak}
-      placeholder="Type to search by species or common name"
-      style={{ marginBottom: '15px' }}
-    />
-    <button
-      className="search-button"
-      disabled={!(selectedAgent && selectedOak)}
-      onClick={getHi}
-    >
+}) =>
+  (
+    <div>
+      <h3>Host-Agent Interactions</h3>
+      <div className="text-muted">If your interaction requires a reference not currently in CODA, please visit the References tab and add it first.</div>
+      <h4>Find an agent</h4>
+      <Select
+        options={agents}
+        onChange={onAgentSelected}
+        value={selectedAgent}
+        placeholder="Type to search by species or common name"
+        style={{ marginBottom: '15px' }}
+      />
+      <h4>Find an oak</h4>
+      <Select
+        options={oaks}
+        onChange={onOakSelected}
+        value={selectedOak}
+        placeholder="Type to search by species or common name"
+        style={{ marginBottom: '15px' }}
+      />
+      <button
+        className="search-button"
+        disabled={!(selectedAgent && selectedOak)}
+        onClick={getHi}
+      >
         Find my interaction!
-    </button>
-    { hi ? (
-      <div>
-        <RadioGroup title="Questionable" selected={hi.questionable} name="questionable" options={BOOLEANS} onChange={onInputChange} />
-        <ButtonGroup title="Situation" selected={hi.situation} name="situation" options={SITUATION} onClick={onMultiInputChange} />
-        <ButtonGroup title="Host Life Stage" selected={hi.hostLifeStage} name="hostLifeStage" options={LIFE_STAGES} onClick={onMultiInputChange} />
-        <TextArea title="Notes" value={hi.notes} name="notes" limit={65535} onChange={onInputChange} />
-        <h4>Range</h4>
-        <CAMap interactionRange={hi.countiesByRegions} agentRange={[]} onMapChange={onMapChange} editable />
-        <h4>References</h4>
-        {hi.bibs.length ? null : <div className="text-muted">Add at least one reference to continue...</div> }
-        <Select options={references} value={hi.bibs} onChange={onBibSelectChange} multi />
-        {hiSymptoms.map(symptom => <HiSymptom symptom={symptom} symptoms={symptoms} key={symptom.id} onSelectChange={onSubsiteSelectChange} onButtonChange={onHisymptomMultiInputChange} onRadioChange={onHisymptomRadioChange} onSymptomChange={onSymptomChange} onSymptomRemove={onSymptomRemove} hiSymptoms={hiSymptoms} />)}
+      </button>
+      { hi ? (
         <div>
-          <b>Add Symptom: </b>
-          {plantParts.map(plantPart => <button onClick={addHiSymptom} key={plantPart + hi.id} value={plantPart}>{plantPart}</button>)}
+          <RadioGroup title="Questionable" selected={hi.questionable} name="questionable" options={BOOLEANS} onChange={onInputChange} />
+          <ButtonGroup title="Situation" selected={hi.situation} name="situation" options={SITUATION} onClick={onMultiInputChange} />
+          <ButtonGroup title="Host Life Stage" selected={hi.hostLifeStage} name="hostLifeStage" options={LIFE_STAGES} onClick={onMultiInputChange} />
+          <TextArea title="Notes" value={hi.notes} name="notes" limit={65535} onChange={onInputChange} />
+          <h4>Range</h4>
+          <CAMap interactionRange={hi.countiesByRegions} agentRange={[]} onMapChange={onMapChange} editable />
+          <h4>References</h4>
+          {hi.bibs.length ? null : <div className="text-muted">Add at least one reference to continue...</div> }
+          <Select options={references} value={hi.bibs} onChange={onBibSelectChange} multi />
+          {hiSymptoms.map(symptom => <HiSymptom symptom={symptom} symptoms={symptoms} key={symptom.id} onSelectChange={onSubsiteSelectChange} onButtonChange={onHisymptomMultiInputChange} onRadioChange={onHisymptomRadioChange} onSymptomChange={onSymptomChange} onSymptomRemove={onSymptomRemove} hiSymptoms={hiSymptoms} />)}
+          <div>
+            <b>Add Symptom: </b>
+            {plantParts.map(plantPart => <button onClick={addHiSymptom} key={plantPart + hi.id} value={plantPart}>{plantPart}</button>)}
+          </div>
+          <button onClick={onHiSubmit} disabled={!hi.bibs.length}>{ newHi ? 'Create' : 'Update'}</button>
         </div>
-        <button onClick={onHiSubmit} disabled={!hi.bibs.length}>{ newHi ? 'Create' : 'Update'}</button>
-      </div>
     ) : null}
 
-  </div>
-);
+    </div>
+  );
 HiEntry.propTypes = {
   agents: PropTypes.array,
   oaks: PropTypes.array,
