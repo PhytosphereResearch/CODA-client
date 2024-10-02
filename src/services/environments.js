ce5897486a77c13bc2959eb57582acfc626580b4
phytosphere <lizbernhardt@gmail.com>
2024-10-02 08:42:53 -0700

    reset-local-host

--- src/services/environments.js
+++ src/services/environments.js
@@ -1,3 +1,4 @@
-// export const url = process.env.NODE_ENV === 'production' ? 'http://coda-env.dudtbymm8n.us-east-2.elasticbeanstalk.com' : 'http://localhost:3000'; //eslint-disable-line
-export const url =
-  "https://rvaypilqrtzkav6o2mc2tlwuba0xypaw.lambda-url.us-east-2.on.aws";
+export const url = process.env.NODE_ENV === 'production' ? 'http://coda-env.dudtbymm8n.us-east-2.elasticbeanstalk.com' : 'http://localhost:3000'; //eslint-disable-line
+
+// export const url =
+  // "https://rvaypilqrtzkav6o2mc2tlwuba0xypaw.lambda-url.us-east-2.on.aws";
