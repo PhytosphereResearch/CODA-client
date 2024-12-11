915e394f61f78eb77db84c30cd38fcfe6619ba2b
Phytosphere <30961253+Phytosphere@users.noreply.github.com>
2024-12-11 14:14:11 -0800

    fixed incorrect key in useSWR (#111)
    * fixed incorrect key in useSWR
    
    * fix broken link and form input helper text
    
    * changes to helper text and and text entry field
    
    * changes to hint text and remove null from dropdown
    
    * change to prevent null froml showing in select dropdown

--- src/components/oaks/Oak.jsx
+++ src/components/oaks/Oak.jsx
@@ -8,51 +8,51 @@ const Oak = () => {
   const { id } = useParams();
   const { isLoading: loading, oak } = useOak(id)
 
-    if (!oak && !loading) {
-      return null;
-    } else if (loading) {
-      return <Spinner />;
-    }
-    let subGenus = (
-      <p>
-        <b>Sub-genus:</b> {oak.subGenus}
-      </p>
-    );
-    let evergreen = (
-      <p>{oak.evergreen}.</p>
-    );
-    let treeForm = (
-      <p>{oak.treeForm} <span> {oak.height}.</span></p>
-    );
-    let range = (
-      <div><b>Range</b><br />
-        {oak.distribution} <br />
-      </div>
-    );
-
-    return (
-      <div>
-        <ScientificName genus={oak.genus} species={oak.species} subSpecies={oak.subSpecies} authority={oak.authority} />
-        { oak.commonName ? <CommonName commonName={oak.commonName} /> : null }
-        { oak.subGenus ? subGenus : null }
-        { oak.evergreen ? evergreen : null }
-        { oak.treeForm || oak.height ? treeForm : null }
-        <p>{oak.leaves}</p>
-        <p>{oak.stems}</p>
-        <p>{oak.acorns}</p>
-
-        { oak.range ? range : null }
-        <p>{oak.hybrids}</p>
-        <p>{oak.varieties}</p>
-        <p>{' '}</p>
-        <CalPhotos genus={oak.genus} species={oak.species} />
-        <p>{' '}</p>
-        <p>
-          <b>Range map:</b> <a href={`https://plants.usda.gov/home/plantProfile?symbol=${oak.usdaCode}`} target="_blank">Search USDA Plants Database</a>
-        </p>
-        { oak.notes ? <Notes notes={oak.notes} /> : null }
-      </div>
-    );
+  if (!oak && !loading) {
+    return null;
+  } else if (loading) {
+    return <Spinner />;
   }
+  let subGenus = (
+    <p>
+      <b>Sub-genus:</b> {oak.subGenus}
+    </p>
+  );
+  let evergreen = (
+    <p>{oak.evergreen}.</p>
+  );
+  let treeForm = (
+    <p>{oak.treeForm} <span> {oak.height}.</span></p>
+  );
+  let range = (
+    <div><b>Range</b><br />
+      {oak.distribution} <br />
+    </div>
+  );
+
+  return (
+    <div>
+      <ScientificName genus={oak.genus} species={oak.species} subSpecies={oak.subSpecies} authority={oak.authority} />
+      {oak.commonName ? <CommonName commonName={oak.commonName} /> : null}
+      {oak.subGenus ? subGenus : null}
+      {oak.evergreen ? evergreen : null}
+      {oak.treeForm || oak.height ? treeForm : null}
+      <p>{oak.leaves}</p>
+      <p>{oak.stems}</p>
+      <p>{oak.acorns}</p>
+
+      {oak.range ? range : null}
+      <p>{oak.hybrids}</p>
+      <p>{oak.varieties}</p>
+      <p>{' '}</p>
+      <CalPhotos genus={oak.genus} species={oak.species} />
+      <p>{' '}</p>
+      <p>
+        <b>Range map:</b> <a href={`https://plants.usda.gov/plant-profile/${oak.usdaCode}`} target="_blank">Search USDA Plants Database</a>
+      </p>
+      {oak.notes ? <Notes notes={oak.notes} /> : null}
+    </div>
+  );
+}
 
 export default Oak;
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,7 +1,7 @@
 const format = (records, idField = "id") =>
   records.map((r) => ({
     value: r[idField],
-    label: `${r.genus} ${r.species} ${r.subSpecies} ${
+    label: `${r.genus} ${r.species} ${r.subSpecies||""} ${
       r.commonName ? `(${r.commonName})` : ""
     }`,
     synId: r.id ? r.id : null,
