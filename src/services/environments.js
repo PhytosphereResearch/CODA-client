f42e75f7526809329cabac0e3e52d042537c78b8
Frances Swiecki-Bernhardt <frances@Francess-MacBook-Pro.local>
2025-01-26 14:38:38 -0800

    fix up production eng

--- src/services/environments.js
+++ src/services/environments.js
@@ -1,4 +1,4 @@
-export const url = import.meta.env.NODE_ENV === 'production' ? 'http://coda-env.dudtbymm8n.us-east-2.elasticbeanstalk.com' : 'http://localhost:3000'; //eslint-disable-line
-
-// export const url =
-  // "https://rvaypilqrtzkav6o2mc2tlwuba0xypaw.lambda-url.us-east-2.on.aws";
+export const url =
+  import.meta.env.NODE_ENV === "production"
+    ? "https://rvaypilqrtzkav6o2mc2tlwuba0xypaw.lambda-url.us-east-2.on.aws"
+    : "http://localhost:3000"; //eslint-disable-line
