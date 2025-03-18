1eb1b24e47fcb58913b4894522b9a893816da1db
Phytosphere <lizbernhardt@gmail.com>
2025-03-17 17:07:17 -0700

    changes to draft code

--- src/components/edit/Agents.jsx
+++ src/components/edit/Agents.jsx
@@ -33,18 +33,19 @@ const blankSynonym = {
 
 const EditAgents = (props) => {
   const [selected, setSelected] = useState();
-  const [selectedSynonym, setSelectedSynonym] = useState({...blankSynonym});
-  const [selectedAgent, setSelectedAgent] = useState({...blankAgent});
+  const [selectedSynonym, setSelectedSynonym] = useState({ ...blankSynonym });
+  const [selectedAgent, setSelectedAgent] = useState({ ...blankAgent });
   const [newAgent, setNewAgent] = useState(true);
-  const { agentFields: fields } = useAgents()
+  const [isRepeat, setIsRepeat] = useState(false);
+  const { agentFields: fields, agents } = useAgents()
   const { getAccessTokenSilently } = useAuth0();
   const { trigger: update, isMutating: loading } = useSWRMutation('/api/agents', addOrUpdateAgent)
 
 
   const resetState = () => {
     setSelected(null);
-    setSelectedAgent({...blankAgent});
-    setSelectedSynonym({...blankSynonym});
+    setSelectedAgent({ ...blankAgent });
+    setSelectedSynonym({ ...blankSynonym });
     setNewAgent(true);
   }
 
@@ -73,7 +74,8 @@ const EditAgents = (props) => {
 
   const onSynonymChange = (e) => {
     const synonym = { ...selectedSynonym, [e.target.name]: e.target.value };
-    console.log("Agents.jsx line 75 synonym=", synonym);
+   const alreadyExists = newAgent && agents.find (agent=>isLikelyRepeat(agent,synonym));
+   setIsRepeat (alreadyExists);
     setSelectedSynonym(synonym);
   }
 
@@ -92,52 +94,49 @@ const EditAgents = (props) => {
       .then(() => resetState());
   }
 
-    const { options } = props;
-      return (
-      <div>
-        <h3>Agents</h3>
-        <Select
-          options={options}
-          onChange={onAgentSelected}
-          value={selected}
-          placeholder="Type to search by species or common name"
-          style={{ marginBottom: '15px' }}
-        />
-        {loading ? <FullScreenSpinner /> : null}
-        {console.log("Agents.jsx line107 options=", options)}
-        {
-          newAgent ? (
-            <div style={{ display: 'flex', gap: '8px' }}>
-              <TextInput title="Genus" value={selectedSynonym.genus} name="genus" onChange={onSynonymChange} />
-              <TextInput title="Species" value={selectedSynonym.species} name="species" onChange={onSynonymChange} />
-              <TextInput title="Sub-species" value={selectedSynonym.subSpecies} name="subSpecies" onChange={onSynonymChange} />
-              <TextInput title="Taxonomic authority" value={selectedSynonym.authority} name="authority" onChange={onSynonymChange} />
-            </div>
+  const { options } = props;
+  return (
+    <div>
+      <h3>Agents</h3>
+      <Select
+        options={options}
+        onChange={onAgentSelected}
+        value={selected}
+        placeholder="Type to search by species or common name"
+        style={{ marginBottom: '15px' }}
+      />
+      {loading ? <FullScreenSpinner /> : null}
+      {console.log("Agents.jsx line107 options=", options)}
+      {
+        newAgent ? (
+          <div style={{ display: 'flex', gap: '8px' }}>
+            <TextInput title="Genus" value={selectedSynonym.genus} name="genus" onChange={onSynonymChange} />
+            <TextInput title="Species" value={selectedSynonym.species} name="species" onChange={onSynonymChange} />
+            <TextInput title="Sub-species" value={selectedSynonym.subSpecies} name="subSpecies" onChange={onSynonymChange} />
+            <TextInput title="Taxonomic authority" value={selectedSynonym.authority} name="authority" onChange={onSynonymChange} />
+          </div>
         ) : null}
-        
-        if (isLikelyRepeat(getAllAgentSynonyms, selectedSynonym)==false) {             
-                 return {<div>Agent already in database, chose agent from dropdown</div>};
-                 }
-            
-        {fields.data ? (
-          <>
-           <EnhancedCreatable title="Type" value={selectedAgent.type} name="type" onChange={(e)=>onSelectChange(e, "type")} options={fields.data.type} />
-            <EnhancedCreatable title="Sub-type" value={selectedAgent.subType} name="subType" onChange={(e)=>onSelectChange(e, "subType")} options={fields.data.subType} />
-            <EnhancedCreatable title="Sub sub-type" value={selectedAgent.subSubType} name="subSubType" onChange={(e)=>onSelectChange(e, "subSubType")} options={fields.data.subSubType} />
+      { isRepeat ? (<div>Agent already exists, select from dropdown</div>):null
+      }
+      {fields.data ? (
+        <>
+          <EnhancedCreatable title="Type" value={selectedAgent.type} name="type" onChange={(e) => onSelectChange(e, "type")} options={fields.data.type} />
+          <EnhancedCreatable title="Sub-type" value={selectedAgent.subType} name="subType" onChange={(e) => onSelectChange(e, "subType")} options={fields.data.subType} />
+          <EnhancedCreatable title="Sub sub-type" value={selectedAgent.subSubType} name="subSubType" onChange={(e) => onSelectChange(e, "subSubType")} options={fields.data.subSubType} />
 
-            <EnhancedCreatable title="Order" name="torder" value={selectedAgent.torder} onChange={(e)=>onSelectChange(e, "torder")} options={fields.data.torder} />
-            <EnhancedCreatable title="Family" name="family" value={selectedAgent.family} onChange={(e)=>onSelectChange(e, "family")} options={fields.data.family} />
-            <RadioGroup title="Most Common" selected={selectedAgent.mostCommon} name="mostCommon" options={BOOLEANS} onChange={onInputChange} />
-            <RadioGroup title="Biotic" selected={selectedAgent.biotic} name="biotic" options={BOOLEANS} onChange={onInputChange} />
- 
-            <RadioGroup title="Ecology" selected={selectedAgent.ecology} name="ecology" options={ECOLOGY} onChange={onInputChange} />
-            <TextInput title="Common Name" value={selectedAgent.commonName} name="commonName" onChange={onInputChange} />
-            <TextArea title="Notes" value={selectedAgent.notes} limit={65535} name="notes" onChange={onInputChange} />
-          </>
-        ): null}
-        <button onClick={onSubmit}>SUBMIT</button>
-      </div>
-    );
+          <EnhancedCreatable title="Order" name="torder" value={selectedAgent.torder} onChange={(e) => onSelectChange(e, "torder")} options={fields.data.torder} />
+          <EnhancedCreatable title="Family" name="family" value={selectedAgent.family} onChange={(e) => onSelectChange(e, "family")} options={fields.data.family} />
+          <RadioGroup title="Most Common" selected={selectedAgent.mostCommon} name="mostCommon" options={BOOLEANS} onChange={onInputChange} />
+          <RadioGroup title="Biotic" selected={selectedAgent.biotic} name="biotic" options={BOOLEANS} onChange={onInputChange} />
+
+          <RadioGroup title="Ecology" selected={selectedAgent.ecology} name="ecology" options={ECOLOGY} onChange={onInputChange} />
+          <TextInput title="Common Name" value={selectedAgent.commonName} name="commonName" onChange={onInputChange} />
+          <TextArea title="Notes" value={selectedAgent.notes} limit={65535} name="notes" onChange={onInputChange} />
+        </>
+      ) : null}
+      <button onClick={onSubmit}>SUBMIT</button>
+    </div>
+  );
 };
 
 EditAgents.propTypes = {
