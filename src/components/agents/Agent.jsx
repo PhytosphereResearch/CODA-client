272536ed0088b93c1b08a61add7b13fc4aca00cc
Phytosphere <lizbernhardt@gmail.com>
2025-08-06 14:54:22 -0700

    auto formatted

--- src/components/agents/Agent.jsx
+++ src/components/agents/Agent.jsx
@@ -6,46 +6,46 @@ import CAMap from '../shared/Map';
 import useAgent from '../../hooks/useAgent';
 
 const Agent = () => {
-const navigate = useNavigate();
-const { id } = useParams();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-const { isLoading: loading, agent } = useAgent(id);
+  const { isLoading: loading, agent } = useAgent(id);
 
   const goToHostInteraction = (e) => {
     const interactionId = e.target.getAttribute('data-interaction');
-    navigate(`/hi/interaction/${interactionId}`, {replace: true});
+    navigate(`/hi/interaction/${interactionId}`, { replace: true });
   }
 
-    if (!agent && !loading) {
-      return null;
-    } else if (loading) {
-      return <Spinner />;
-    }
+  if (!agent && !loading) {
+    return null;
+  } else if (loading) {
+    return <Spinner />;
+  }
 
-    const hosts = agent?.hosts && (
-      <div>
-        <b>Hosts: </b>
-        {agent.hosts.map((h, index) => (
-          <span key={h.genus + h.species}>
-            <a style={{ cursor: 'pointer' }} onClick={goToHostInteraction}>
-              <i data-interaction={h.interactionId}>
-                {h.genus} {h.species}{h.subSpecies ? ' ' : ''}{h.subSpecies}
-              </i>
-            </a>
-            {index < agent.hosts.length - 1 ? ', ' : ''}
-          </span>
-        ))}
-        <div className="text-muted">Click an oak to see interaction details</div>
-      </div>
-    );
+  const hosts = agent?.hosts && (
+    <div>
+      <b>Hosts: </b>
+      {agent.hosts.map((h, index) => (
+        <span key={h.genus + h.species}>
+          <a style={{ cursor: 'pointer' }} onClick={goToHostInteraction}>
+            <i data-interaction={h.interactionId}>
+              {h.genus} {h.species}{h.subSpecies ? ' ' : ''}{h.subSpecies}
+            </i>
+          </a>
+          {index < agent.hosts.length - 1 ? ', ' : ''}
+        </span>
+      ))}
+      <div className="text-muted">Click an oak to see interaction details</div>
+    </div>
+  );
 
-    return (
-      <div>
-        <ScientificName genus={agent.primarySynonym?.genus} species={agent.primarySynonym?.species} subSpecies={agent.primarySynonym?.subSpecies} authority={agent.primarySynonym?.authority} />
-          <div style={{ display: 'grid', gridTemplateColumns: '2fr 1fr' }}>
-            <div>
+  return (
+    <div>
+      <ScientificName genus={agent.primarySynonym?.genus} species={agent.primarySynonym?.species} subSpecies={agent.primarySynonym?.subSpecies} authority={agent.primarySynonym?.authority} />
+      <div style={{ display: 'grid', gridTemplateColumns: '2fr 1fr' }}>
+        <div>
           <p />
-          { agent.commonName ? <CommonName commonName={agent.commonName} /> : null }
+          {agent.commonName ? <CommonName commonName={agent.commonName} /> : null}
           <CalPhotos genus={agent.primarySynonym?.genus} species={agent.primarySynonym?.species} />
           <p />
           <Synonyms synonyms={agent.otherSynonyms} />
@@ -69,7 +69,7 @@ const { isLoading: loading, agent } = useAgent(id);
         </div>
         <div><b>Reported range</b> <br />
           <CAMap interactionRange={[]} agentRange={agent.rangeData} />
-        </div>     
+        </div>
       </div>
     </div>
   );
