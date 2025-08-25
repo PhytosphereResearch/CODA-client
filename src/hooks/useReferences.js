97611c92281211bc61c19babc1d806bb1a7314f7
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2025-08-25 12:33:28 -0700

    sort symptoms; use shared sort util for dropdowns

--- src/hooks/useReferences.js
+++ src/hooks/useReferences.js
@@ -1,16 +1,19 @@
 import useSWR from "swr";
 import { getReferences } from "../services/interactions";
+import sortByLabel from "../utils/sortByLabel";
 
 export default function useReferences() {
   const { data, error, isLoading } = useSWR("/api/references", getReferences, {
     revalidateOnFocus: false,
   });
 
-  const formattedReferences = data?.map((r) => ({
-    ...r,
-    value: r.id,
-    label: r.description||`${r.id}`,
-  })).sort((recordA, recordB) => recordA.label.localeCompare(recordB.label));
+  const formattedReferences = data
+    ?.map((r) => ({
+      ...r,
+      value: r.id,
+      label: r.description || `${r.id}`,
+    }))
+    .sort(sortByLabel);
 
   return {
     references: data || [],
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,10 +1,14 @@
+import sortByLabel from "./sortByLabel";
+
 const format = (records, idField = "id") =>
-  records.map((r) => ({
-    value: r[idField],
-    label: `${r.genus} ${r.species} ${r.subSpecies||""} ${
-      r.commonName ? `(${r.commonName})` : ""
-    }`,
-    synId: r.id ? r.id : null,
-  })).sort((recordA, recordB) => recordA.label.localeCompare(recordB.label));
+  records
+    .map((r) => ({
+      value: r[idField],
+      label: `${r.genus} ${r.species} ${r.subSpecies || ""} ${
+        r.commonName ? `(${r.commonName})` : ""
+      }`,
+      synId: r.id ? r.id : null,
+    }))
+    .sort(sortByLabel);
 
 export default format;
