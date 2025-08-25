358a88234b0e221845ca908fb05964f1e97e27b6
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2025-08-25 12:38:34 -0700

    sort hosts by scientific name

--- src/components/agents/Agent.jsx
+++ src/components/agents/Agent.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { useNavigate, useParams } from 'react-router';
-import { ScientificName, CommonName, Synonyms, CalPhotos, Notes, AgentTaxonomy, DefaultCitation, AuditRecord } from '../shared/partials';
-import { Spinner } from '../shared/shapes';
-import CAMap from '../shared/Map';
-import useAgent from '../../hooks/useAgent';
+import React from "react";
+import { useNavigate, useParams } from "react-router";
+import {
+  ScientificName,
+  CommonName,
+  Synonyms,
+  CalPhotos,
+  Notes,
+  AgentTaxonomy,
+  DefaultCitation,
+  AuditRecord,
+} from "../shared/partials";
+import { Spinner } from "../shared/shapes";
+import CAMap from "../shared/Map";
+import useAgent from "../../hooks/useAgent";
+import sortByScientificName from "../../utils/sortByScientificName";
 
 const Agent = () => {
   const navigate = useNavigate();
@@ -12,9 +22,9 @@ const Agent = () => {
   const { isLoading: loading, agent } = useAgent(id);
 
   const goToHostInteraction = (e) => {
-    const interactionId = e.target.getAttribute('data-interaction');
+    const interactionId = e.target.getAttribute("data-interaction");
     navigate(`/hi/interaction/${interactionId}`, { replace: true });
-  }
+  };
 
   if (!agent && !loading) {
     return null;
@@ -25,14 +35,16 @@ const Agent = () => {
   const hosts = agent?.hosts && (
     <div>
       <b>Hosts: </b>
-      {agent.hosts.map((h, index) => (
+      {agent.hosts.sort(sortByScientificName).map((h, index) => (
         <span key={h.genus + h.species}>
-          <a style={{ cursor: 'pointer' }} onClick={goToHostInteraction}>
+          <a style={{ cursor: "pointer" }} onClick={goToHostInteraction}>
             <i data-interaction={h.interactionId}>
-              {h.genus} {h.species}{h.subSpecies ? ' ' : ''}{h.subSpecies}
+              {h.genus} {h.species}
+              {h.subSpecies ? " " : ""}
+              {h.subSpecies}
             </i>
           </a>
-          {index < agent.hosts.length - 1 ? ', ' : ''}
+          {index < agent.hosts.length - 1 ? ", " : ""}
         </span>
       ))}
       <div className="text-muted">Click an oak to see interaction details</div>
@@ -41,16 +53,30 @@ const Agent = () => {
 
   return (
     <div>
-      <ScientificName genus={agent.primarySynonym?.genus} species={agent.primarySynonym?.species} subSpecies={agent.primarySynonym?.subSpecies} authority={agent.primarySynonym?.authority} />
-      <div style={{ display: 'grid', gridTemplateColumns: '2fr 1fr' }}>
+      <ScientificName
+        genus={agent.primarySynonym?.genus}
+        species={agent.primarySynonym?.species}
+        subSpecies={agent.primarySynonym?.subSpecies}
+        authority={agent.primarySynonym?.authority}
+      />
+      <div style={{ display: "grid", gridTemplateColumns: "2fr 1fr" }}>
         <div>
           <p />
-          {agent.commonName ? <CommonName commonName={agent.commonName} /> : null}
-          <CalPhotos genus={agent.primarySynonym?.genus} species={agent.primarySynonym?.species} />
+          {agent.commonName ? (
+            <CommonName commonName={agent.commonName} />
+          ) : null}
+          <CalPhotos
+            genus={agent.primarySynonym?.genus}
+            species={agent.primarySynonym?.species}
+          />
           <p />
           <Synonyms synonyms={agent.otherSynonyms} />
           <p />
-          {agent.synonyms?.map(synonym => (synonym.notes ? <div key={synonym.notes}>{synonym.notes}</div> : null))}
+          {agent.synonyms?.map((synonym) =>
+            synonym.notes ? (
+              <div key={synonym.notes}>{synonym.notes}</div>
+            ) : null
+          )}
           <p />
           <AgentTaxonomy agent={agent} />
           <p />
@@ -59,16 +85,19 @@ const Agent = () => {
           {agent.notes ? <Notes notes={agent.notes} /> : null}
           <p>
             <b>Record history:</b>
-            {agent.originalCodaRecord ? <DefaultCitation />  : null}
-            {agent.auditRecords.map((auditRecord) => <AuditRecord key={auditRecord.id} auditRecord={auditRecord} /> )}
+            {agent.originalCodaRecord ? <DefaultCitation /> : null}
+            {agent.auditRecords.map((auditRecord) => (
+              <AuditRecord key={auditRecord.id} auditRecord={auditRecord} />
+            ))}
           </p>
         </div>
-        <div><b>Reported range</b> <br />
+        <div>
+          <b>Reported range</b> <br />
           <CAMap interactionRange={[]} agentRange={agent.rangeData} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Agent;
